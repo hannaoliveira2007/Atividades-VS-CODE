0d01ff3cae6a7f311f096731bce97c414b2a0a54
hannaoliveira2007 <hannaboliveiras@gmail.com>
2025-05-06 11:03:07 -0300

    7.py

--- 7.py
+++ 7.py
@@ -1,14 +1,11 @@
 #Faça um algoritmo que leia dois valores booleanos (lógicos) e determine se
 # ambos são VERDADEIRO ou FALSO. Sendo o numero 1 Verdadeiro e 0 Falso
 
-#x = bool(input("Valor 1:"))
-#y = bool(input("Valor 2:"))
-
 a = 3
 b = 4
-c = a < b   # c recebe o valor da comparação a < b
-d = a > b   # d recebe o valor da comparação a > b
-e = a == b  # e recebe o valor da comparação a == b
+c = a < b 
+d = a > b   
+e = a == b 
 
 print("Valor de c: ", c)
 print("Valor de d: ", d)
