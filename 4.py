b3344066a265910a79a080ae9137e61adb657d1d
hannaoliveira2007 <hannaboliveiras@gmail.com>
2025-05-06 11:05:31 -0300

    4.py

---
+++ 4.py
@@ -0,0 +1,6 @@
+#Faça um algoritmo que receba um número inteiro e imprima na tela o seu antecessor e o seu sucessor.
+
+x = float(input("Diga um número: "))
+
+print("Seu antecessor é:", x-1)
+print("Seu sucessor é: ", x+1)
